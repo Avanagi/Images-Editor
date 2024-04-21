06307ca2c3e02eeb85477f2fc7a2c9b037489cee
Roman Gorelov <avanagi.payment@gmail.com>
2024-04-21 19:19:54 +0300

    Contours

---
+++ Kenny/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ImageMagick;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string inputFile = "Table.png";
+        string outputFile = "res.png";
+
+        using (MagickImage image = new MagickImage(inputFile))
+        {
+            image.CannyEdge();
+            image.Write(outputFile);
+        }
+    }
+}
