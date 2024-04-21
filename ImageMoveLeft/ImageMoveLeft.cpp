b4d6a1d92e3becdf01f453f7300ace949e6d1b58
Roman Gorelov <avanagi.payment@gmail.com>
2024-04-21 21:17:21 +0300

    Brightness Correction And Image Move

---
+++ ImageMoveLeft/ImageMoveLeft.cpp
@@ -0,0 +1,25 @@
+﻿#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main() {
+    Mat image = imread("Images\\Screen.png");
+
+    if (image.empty()) {
+        cout << "Не удалось загрузить изображение." << endl;
+        return -1;
+    }
+    
+    imshow("Исходное изображение", image);
+
+    //матрица сдвига размером 2x3. Первая строка [1, 0, -400] отвечает за горизонтальный сдвиг (на -400 пикселей влево), 
+    // а вторая строка [0, 1, 0] за вертикальный сдвиг (без сдвига).
+    Mat shiftMatrix = (Mat_<float>(2, 3) << 1, 0, -400, 0, 1, 0);
+    Mat shiftedImage;
+    warpAffine(image, shiftedImage, shiftMatrix, image.size());
+
+    imshow("Сдвинутое изображение", shiftedImage);
+    waitKey(0);
+}
---
+++ Perfect/Perfect.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main() {
+    Mat image = imread("Images\\sc.jpg");
+    imshow("First", image);
+
+    if (image.empty()) {
+        cout << "Не удалось загрузить изображение." << endl;
+        return -1;
+    }
+
+    int Rmax = 255;
+    int Gmax = 255;
+    int Bmax = 255; 
+    for (int y = 0; y < image.rows; ++y) {
+        for (int x = 0; x < image.cols; ++x) {
+            Vec3b& pixel = image.at<Vec3b>(y, x);
+
+            pixel[0] = pixel[0] * (255 / Rmax);
+            pixel[1] = pixel[1] * (255 / Gmax);
+            pixel[2] = pixel[2] * (255 / Bmax);
+        }
+    }
+
+    namedWindow("Коррекция бликов", WINDOW_AUTOSIZE);
+    imshow("Коррекция бликов", image);
+    waitKey(0);
+
+}
